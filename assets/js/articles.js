03d76d4427b899a26902e074a89aa21f91885097
Fabien Lamotte <devWebFabienLamottePro@outlook.fr>
2021-03-24 21:49:45 +0100

    Création d'un fichier js listant tout les articles nécessaire pour leur ajout

---
+++ assets/js/articles.js
@@ -0,0 +1,130 @@
+var articles = [
+    {
+        'id':1,
+        'image' : 'flacons.jpg',
+        'title': 'Pack "Collection" Compagnie du Bicarbonate',
+        'prixDeBase': 19.94,
+        'prixRemise' : 0,
+        'colorButton': '#79daf9'
+    },
+    {
+        'id':2,
+        'image' : 'percarbonate.jpg',
+        'title': 'Percarbonate de sodium (de soude) - Flacon rechargeable 1,1 kg (10 % gratuits)',
+        'prixDeBase': 8.95,
+        'prixRemise' : 6.46,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':3,
+        'image' : 'miniguide.png',
+        'title': 'OFFERT : Mini-Guide des Principales Utilisations du Bicarbonate',
+        'prixDeBase': 0,
+        'prixRemise' : 0,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':4,
+        'image' : 'bicarbonate-alimentaire.jpg',
+        'title': 'Bicarbonate alimentaire extra-fin - Sac «Open-up» 3 kg',
+        'prixDeBase': 12.90,
+        'prixRemise' : 9.95,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':5,
+        'image' : 'cristaux-soude.jpg',
+        'title': 'Cristaux de Soude - 1 kg',
+        'prixDeBase': 5.60,
+        'prixRemise' : 4.90,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':6,
+        'image' : 'savon-marseille.jpg',
+        'title': 'Savon de Marseille SANS HUILE DE PALME - 600 g',
+        'prixDeBase': 5.16,
+        'prixRemise' : 4.60,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':7,
+        'image' : 'vinaigre-blanc.jpg',
+        'title': 'Vinaigre blanc français d\'origine naturelle concentré à 12 °- Arôme citron - 5 litres',
+        'prixDeBase': 15.90,
+        'prixRemise' : 10.90,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':,
+        'image' : 'bipoux.jpg',
+        'title': 'Bi-Poux® 400 g - Poudre d\'hygiène et de contrôle de l\'hygrométrie',
+        'prixDeBase': 19.90,
+        'prixRemise' : 15.60,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':9,
+        'image' : 'gel-vegetal.jpg',
+        'title': 'Gel végétal naturel au vinaigre 14° à la menthe 1 litre',
+        'prixDeBase': 4.96,
+        'prixRemise' : 3.95,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':10,
+        'image' : 'niaouli-nibouton.jpg',
+        'title': '« Niaouli ni bouton » - Peau mixte à grasse',
+        'prixDeBase': 4.50,
+        'prixRemise' : 0,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':11,
+        'image' : 'bicarbonate-alimentaire-extra-fine.jpg',
+        'title': 'Bicarbonate alimentaire - Granulométrie extra-fine (0-130 µm) - Sac de 25 Kg',
+        'prixDeBase': 65.00,
+        'prixRemise' : 48.99,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':12,
+        'image' : 'copeaux-savon-marseille.jpg',
+        'title': 'Copeaux de Savon de Marseille au BICARBONATE - 1 Kg',
+        'prixDeBase': 11.90,
+        'prixRemise' : 10.60,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':13,
+        'image' : 'argile-rose.png',
+        'title': 'Argile Rose Kaolinite ultra-ventillée - Laboratoire Argiletz - 200 g',
+        'prixDeBase': 5.60,
+        'prixRemise' : 4.92,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':14,
+        'image' : 'savon-liquide-marseille.jpg',
+        'title': 'Savon liquide de Marseille à l’huile essentielle de Lavandin - 5L',
+        'prixDeBase': 37.81,
+        'prixRemise' : 0,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':15,
+        'image' : 'gel-vegetal-1l.jpg',
+        'title': 'Gel végétal naturel au bicarbonate de soude 1 litre',
+        'prixDeBase': 4.96,
+        'prixRemise' : 3.95,
+        'colorButton': '#FFFFFF'
+    },
+    {
+        'id':16,
+        'image' : '2-lingettes-lavable.jpg',
+        'title': '2 lingettes lavables en coton "Douceur du Nil"',
+        'prixDeBase': 3.60,
+        'prixRemise' : 2.90,
+        'colorButton': '#FFFFFF'
+    },
+];
